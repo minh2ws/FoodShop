a44829c77a49ae11763894ad75262756db60c577
HuyVDSE <nhoxroney9x1@gmail.com>
2021-02-18 23:14:32 +0700

    update Emp DTO

--- employee-management-asp-winform-master/employee-management-asp-winform/DTO/EmployeeDTO.cs
+++ employee-management-asp-winform-master/employee-management-asp-winform/DTO/EmployeeDTO.cs
@@ -8,11 +8,10 @@ namespace employee_management_asp_winform.DTO
 {
     class EmployeeDTO
     {
-        public string empId { get; set; }
-        public string fullName { get; set; }
-        public string address { get; set; }
-        public string phone { get; set; }
-        public string email { get; set; }
+        public string idEmployee { get; set; }
+        public string name { get; set; }
+        public string password { get; set; }
+        public string role { get; set; }
         public bool status { get; set; }
     }
 }
