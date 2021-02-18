b9ef6e00ebf64774c09481c7909dd8f090490fb0
HuyVDSE <nhoxroney9x1@gmail.com>
2021-02-18 22:35:36 +0700

    add emp management desktop app

---
+++ employee-management-asp-winform-master/employee-management-asp-winform/DTO/EmployeeDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace employee_management_asp_winform.DTO
+{
+    class EmployeeDTO
+    {
+        public string empId { get; set; }
+        public string fullName { get; set; }
+        public string address { get; set; }
+        public string phone { get; set; }
+        public string email { get; set; }
+        public bool status { get; set; }
+    }
+}
---
+++ employee-management-asp-winform-master/employee-management-asp-winform/MainFrame.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using employee_management_asp_winform.DTO;
+using Newtonsoft.Json;
+
+namespace employee_management_asp_winform
+{
+    public partial class MainFrame : Form
+    {
+        
+        public MainFrame()
+        {
+            InitializeComponent();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44329/");
+            HttpResponseMessage responseMessage = client.GetAsync("api/employees/find_all").Result;
+            var empList = responseMessage.Content.ReadAsStringAsync();
+            List<EmployeeDTO> data = JsonConvert.DeserializeObject<List<EmployeeDTO>>(empList.Result);
+            foreach (var emp in data)
+            {
+                dataGridView1.Rows.Add(emp.empId, emp.fullName, emp.email, emp.phone, emp.status);
+            }
+        }
+
+        private void btnFindAll_Click(object sender, EventArgs e)
+        {
+            dataGridView1.Rows.Clear();
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("https://localhost:44329/");
+            HttpResponseMessage responseMessage = client.GetAsync("api/employees/find_all").Result;
+            var empList = responseMessage.Content.ReadAsStringAsync();
+            List<EmployeeDTO> data =JsonConvert.DeserializeObject<List<EmployeeDTO>>(empList.Result);
+            foreach (var emp in data)
+            {
+                dataGridView1.Rows.Add(emp.empId,emp.fullName,emp.email,emp.phone,emp.status);
+            }
+            
+        }
+        
+    }
+}
