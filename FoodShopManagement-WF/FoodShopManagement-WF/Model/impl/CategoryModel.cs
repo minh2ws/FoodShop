b22c04f21ec8b1bf12c4cfb60d745914025a1562
longphamit <longphamjustdoit@gmail.com>
2021-03-17 14:15:48 +0700

    load category and by to datagridview

---
+++ FoodShopManagement-WF/FoodShopManagement-WF/Model/impl/CategoryModel.cs
@@ -0,0 +1,27 @@
+﻿using DTO;
+using FoodShopManagement_WF.Util;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FoodShopManagement_WF.Model.impl
+{
+    class CategoryModel : ICategoryModel
+    {
+        public List<TblCategoryDTO> getAll()
+        {
+            HttpResponseMessage responseMessage = ApiConnection.loadGetJsonObject("category/loadCategory", Program.TokenGlobal);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var listFromAPI= responseMessage.Content.ReadAsStringAsync();
+                List<TblCategoryDTO> listResult = JsonConvert.DeserializeObject<List<TblCategoryDTO>>(listFromAPI.Result);
+                return listResult;
+            }
+            return null;
+        }
+    }
+}
---
+++ FoodShopManagement-WF/FoodShopManagement-WF/Presenter/impl/CategoryPresenter.cs
@@ -0,0 +1,36 @@
+﻿using DTO;
+using FoodShopManagement_WF.Model;
+using FoodShopManagement_WF.Model.impl;
+using FoodShopManagement_WF.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodShopManagement_WF.Presenter.impl
+{
+    class CategoryPresenter : ICategoryPresenter
+    {
+        private ICategoryModel categoryModel = new CategoryModel();
+        private frmWarehouse_V2 form;
+        public CategoryPresenter (frmWarehouse_V2 form)
+        {
+            this.form = form;
+        }
+        public void getAll()
+        {
+            List<TblCategoryDTO> categoryDTOs= categoryModel.getAll();
+            BindingSource bindingSource = new BindingSource() {
+                DataSource = categoryDTOs
+            };
+            form.GetDataGridViewCategory().DataSource = bindingSource;
+            form.GetBindingNavigatorCategory().BindingSource = bindingSource;
+            form.getIdCategory().DataBindings.Clear();
+            form.getNameCategory().DataBindings.Clear();
+            form.getIdCategory().DataBindings.Add("Text", bindingSource, "idCategory");
+            form.getNameCategory().DataBindings.Add("Text", bindingSource, "name");
+        }
+    }
+}
