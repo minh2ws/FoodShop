955a7f8c39187e37f5eb8f3cba34471dc60fb540
longphamit <longphamjustdoit@gmail.com>
2021-03-21 14:11:05 +0700

    fix conflict

--- FoodShopManagement-WF/FoodShopManagement-WF/Util/MessageUtil.cs
+++ FoodShopManagement-WF/FoodShopManagement-WF/Util/MessageUtil.cs
@@ -10,8 +10,12 @@ namespace FoodShopManagement_WF.Util
     {
         public static string ERROR = "Error processing";
         public static string SAVE_SUCCESS = "Save success";
+        public static string INVALID_CUS_NAME = "Customer Name must from 0 to 50 chars!!";
+        public static string INVALID_CUS_PHONE = "Customer Phone must be number!!";
+        public static string INVALID_CUS_ADDRESS = "Customer Address must from 0 to 200 chars";
         public static string DELETE_CONFIRM = "Do you want to delete ?";
         public static string DELETE_SUCCESS = "Delete success";
         public static string DELETE_ALREADY = "Delete Already";
+        
     }
 }
